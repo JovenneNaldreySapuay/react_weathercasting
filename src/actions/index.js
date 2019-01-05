c7d25af1051a9ff446aa3b07b8958caeae638fd3
jovenne <jovenneregs@gmail.com>
2019-01-05 22:06:43 +0800

    initial commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'a026aeaa1ac9684c33b8af6b516f38f9';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
